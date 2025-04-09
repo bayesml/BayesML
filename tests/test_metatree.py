3be614efa48c9c5fec1d10f36f4be0143dd91d97
Yuta Nakahara <92844770+yuta-nakahara@users.noreply.github.com>
2025-04-09 11:15:39 +0900

    Add categorical

--- tests/test_metatree.py
+++ tests/test_metatree.py
@@ -9,6 +9,7 @@ sys.path.append(parent_path)
 
 from bayesml import metatree
 from bayesml import linearregression
+from bayesml import categorical
 
 import numpy as np
 
@@ -94,6 +95,62 @@ def test_metatree_linearregression_batch_pred(metatree_sample_data):
         0.29176254, 0.25486811, 0.24659016, 0.18370341, 0.36756198]])
     # check if the prediction densities are close to the desired values
     assert np.all(np.isclose(pred_densities, desireble_pred_densities)), f"Prediction densities are incorrect: {pred_densities} != {desireble_pred_densities}"
+
+def test_metatree_categorical_batch_pred(metatree_sample_data):
+    x_continuous = metatree_sample_data['x_continuous']
+    x_categorical = metatree_sample_data['x_categorical']
+    y_continuous = metatree_sample_data['y_continuous']
+    y_categorical = metatree_sample_data['y_categorical']
+
+    # initialise the model
+    model = metatree.LearnModel(
+        c_dim_continuous=3,
+        c_dim_categorical=2,
+        SubModel=categorical,
+        sub_constants={'c_degree':3},
+    )
+    # update the posterior distribution
+    model.update_posterior(
+        x_continuous=x_continuous,
+        x_categorical=x_categorical,
+        y=y_categorical,
+        random_state=123,
+    )
+    # calculate the predictive distribution
+    model.calc_pred_dist(
+        x_continuous=x_continuous,
+        x_categorical=x_categorical,
+    )
+    
+    ##############################
+    # test on prediction values
+    ##############################
+    # calculate the prediction values
+    pred_values = model.make_prediction(loss='0-1')
+    # desired prediction values
+    # the values have been calculated using the same model and parameters, but as sequential predictions
+    desireble_pred_values = np.array(
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+    # check if the prediction values are close to the desired values
+    assert np.all(np.isclose(pred_values, desireble_pred_values)), f"Prediction values are incorrect: {pred_values} != {desireble_pred_values}"
+
+    # calculate the prediction values
+    pred_values = model.make_prediction(loss='KL')
+    # desired prediction values
+    # the values have been calculated using the same model and parameters, but as sequential predictions
+    desireble_pred_values = np.array(
+        [[0.35627564, 0.54269806, 0.1010263 ],
+         [0.65728076, 0.27916022, 0.06355903],
+         [0.76414301, 0.17634316, 0.05951382],
+         [0.76537173, 0.1754697 , 0.05915857],
+         [0.76452273, 0.17636313, 0.05911414],
+         [0.61047234, 0.3179039 , 0.07162376],
+         [0.76485687, 0.17597582, 0.0591673 ],
+         [0.74948527, 0.19151863, 0.0589961 ],
+         [0.37772062, 0.52622695, 0.09605243],
+         [0.5943782 , 0.33592791, 0.06969389]])
+    # check if the prediction values are close to the desired values
+    assert np.all(np.isclose(pred_values, desireble_pred_values)), f"Prediction values are incorrect: {pred_values} != {desireble_pred_values}"
 
 if __name__ == "__main__":
     pytest.main()
