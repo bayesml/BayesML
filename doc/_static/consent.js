2bf10e7df03f9c2324d0d5a63e4fbf17f3f8aeba
Yuta Nakahara <92844770+yuta-nakahara@users.noreply.github.com>
2026-09-01 12:08:37 +0900

    Load GA from _static/consent.js

---
+++ doc/_static/consent.js
@@ -0,0 +1,25 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+// analytics_storage denied for EU/EEA and UK only
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
+  'analytics_storage': 'granted'
+});
+gtag('consent', 'default', {
+  'region': ['AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR',
+             'HU','IS','IE','IT','LV','LI','LT','LU','MT','NL','NO','PL',
+             'PT','RO','SK','SI','ES','SE','GB','CH'],
+  'analytics_storage': 'denied'
+});
+
+gtag('js', new Date());
+gtag('config', 'G-59F6KL8C5D');
+
+// Load gtag.js dynamically
+var s = document.createElement('script');
+s.async = true;
+s.src = 'https://www.googletagmanager.com/gtag/js?id=G-59F6KL8C5D';
+document.head.appendChild(s);
